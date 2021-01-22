5f93c334ed0632e4e8fde83b9a04524b70de5fea
Chuck Tang <j316chuck@berkeley.edu>
2021-01-22 13:45:06 -0800

    fixed big bug in recording alphas

--- parallel_runner.m
+++ parallel_runner.m
@@ -1,7 +1,7 @@
 N = 100;
 interval = 5;
 n = N / interval; 
-num_workers = 20; 
+num_workers = 12; 
 parfor (i = 1:n,num_workers)
     st = (i-1) * interval + 1; en = i * interval; 
     fprintf("Running experiments %d %d\n", st, en);
